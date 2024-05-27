a1f6a17120a5396737dfcb7ea7010b07bc9aacf6
LLetzel <lucasdiasletzel@gmail.com>
2024-05-26 23:05:46 -0300

    26/05

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Para funcionar a extensão desse arquivo deve ser .php</title>
+
+    <style>
+        /* Seu css funciona aqui!*/ 
+    </style>
+</head>
+<body>
+    <?php
+        // Seu php funciona aqui e em qualquer lugar desse arquivo!!
+    ?>
+</body>
+    <script>
+        // Seu javascript funciona aqui!
+    </script>
+</html>
